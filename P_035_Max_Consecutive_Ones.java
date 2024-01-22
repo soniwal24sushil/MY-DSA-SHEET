0e54d8659149a5fde6b75aa61e828d2ead0ddbe9
soniwal24sushil <soniwal24sushil@gmail.com>
2024-01-22 08:56:21 +0530

    LC: 485. Max Consecutive Ones

---
+++ P_035_Max_Consecutive_Ones.java
@@ -0,0 +1,25 @@
+public class P_035_Max_Consecutive_Ones {
+
+    // Todo: https://leetcode.com/problems/max-consecutive-ones
+
+    // * T.C = O(n) & S.C = O(1).
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, c = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                c++;
+            } else {
+                max = Math.max(max, c);
+                c = 0;
+            }
+        }
+        max = Math.max(max, c);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 0, 1, 1, 1 };
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+}
