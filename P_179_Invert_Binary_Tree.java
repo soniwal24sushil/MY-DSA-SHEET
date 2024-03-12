bdf85d191f487902daa03304b929c8fd78a3781d
soniwal24sushil <soniwal24sushil@gmail.com>
2024-03-12 09:58:00 +0530

    LC: 226. Invert Binary Tree

---
+++ P_179_Invert_Binary_Tree.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import TreeClasses.TreeNode;
+
+public class P_179_Invert_Binary_Tree {
+
+    // Todo: https://leetcode.com/problems/invert-binary-tree/
+
+    // * T.C = O(n) & S.C = O(n).
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(invertTree(root));
+    }
+}
